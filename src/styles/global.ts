b2e3ad970be0b216b74093be3cae2058ae5b9e05
AbmaelFerreira <abmael.ninha@gmail.com>
2025-01-16 14:03:06 -0400

    feaat(Adicionando cores, configurando Eslint e Rotas)

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,29 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-      }
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
+  }
 
 body {
-       background: #333;
-       color: #fff;
+       background: ${props => props.theme['gray-900']};
+       color:${props => props.theme['gray-300']};
      }
+
+body, input, textarea, button {
+  font-family: "Roboto", serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 1rem;
+  font-variation-settings:
+    "wdth" 100;
+}
 `;

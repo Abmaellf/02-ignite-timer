26ebb498f2a829dac75e0689f9b20a11caaff162
AbmaelFerreira <abmael.ninha@gmail.com>
2025-01-22 22:31:55 -0400

    feat(2-1-2-6-Aprimorando os inputs )

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,25 @@ export function Home() {
             <form action="">
                  <FormContainer>
                      <label htmlFor="">Vou trabalhar em</label>
-                     <TaskInput id="task" placeholder="Dê um nome para o seu projeto"/>
+
+                     <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto"/>
+
+                     <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2"/>
+                        <option value="Projeto 3"/>
+                        <option value="Banana"/>
+                     </datalist>
  
                      <label htmlFor="">Durante</label>
-                     <MinutesAmountInput type="number" id="minutesAmount"/>
+
+                     <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmount"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
  
                      <span>Minutos.</span>
                  </FormContainer>
